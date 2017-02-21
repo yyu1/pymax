25460646fd9cd99e93b8b1086c3b15840d888f5a
Yifan Yu <yifany@ucla.edu>
2017-02-20 19:14:20 -0800

    debug

--- pre/writeswd.py
+++ pre/writeswd.py
@@ -57,7 +57,7 @@ def writeswd(outFileName, trainSamples, imageNames, extractionValues):
 
 		#write extraction values
 		for index in range(nsamples):
-			print(classname[index])
+			print(str(classname[index]))
 			print(',{:.2f},{:.2f}'.format(xcoord[index],ycoord[index]))
 			outline = classname[index] + ',{:.2f},{:.2f}'.format(xcoord[index],ycoord[index])
 			for image in extractionValues:
